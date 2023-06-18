ce2850a72028db7ab647e5c4992bd9e39727ab96
David Sweet <dsweet@andamooka.org>
2023-06-18 08:45:33 -0400

    X_sigma

--- log_linear/log_linear.py
+++ log_linear/log_linear.py
@@ -1,40 +1,47 @@
 import numpy as np
 import statsmodels.api as sm
 
+
 class LogLinear:
     def __init__(self, tolerance=1e-4, max_iterations=1000):
         self.tolerance = tolerance
         self.max_iterations = max_iterations
 
-    def fit(self, X, y, b_trace=False):
-        sigma2 = np.ones(shape=data.y.shape)
-        if b_trace:
-            llf_trace = []
+    def fit(self, X, X_sigma, y):
+        sigma2 = np.ones(shape=y.shape)
+
+        X_sigma = np.concatenate(
+            (
+                np.ones(shape=(X_sigma.shape[0], 1)),
+                X_sigma,
+            ),
+            axis=1,
+        )
+
+        llf_trace = []
         llf_prev = -1e99
         alpha = 1
         params = None
         for i_iter in range(self.max_iterations):
-            # TODO: why not 1/sigma?
-            mod = sm.WLS(data.y, X, weights=1 / sigma2)
+            mod = sm.WLS(y, X, weights=1 / sigma2)
             fit = mod.fit()
-            if b_trace:
-                llf_trace.append(fit.llf)
+            llf_trace.append(fit.llf)
             conv_check = abs(fit.llf - llf_prev) / abs(fit.llf)
             if conv_check < self.tolerance:
-                if b_trace:
-                    return llf_trace
-                return fit.params[1], fit.tvalues[1]
+                self.fit_ = fit
+                self.coef_ = fit.params
+                self.cov_ = fit.cov_params()
+                return llf_trace
             llf_prev = fit.llf
             if params is None:
                 params = fit.params
             else:
                 params += alpha * (fit.params - params)
                 alpha = max(0.1, 0.9 * alpha)
-            eps = data.y - X @ params
+            eps = y - X @ params
+
             log_eps2 = np.log(eps**2)
-            mod_var = sm.OLS(log_eps2, X)
+            mod_var = sm.OLS(log_eps2, X_sigma)
             fit_var = mod_var.fit()
-            sigma2 = np.exp(fit_var.predict(X))
-        assert (
-            False
-        ), f"Reached self.max_iterations = {self.max_iterations} llf = {fit.llf} conv_check = {conv_check}"
+            sigma2 = np.exp(fit_var.predict(X_sigma))
+        assert False, f"Reached self.max_iterations = {self.max_iterations} llf = {fit.llf} conv_check = {conv_check}"
--- tests/test_log_linear.py
+++ tests/test_log_linear.py
@@ -1,7 +1,36 @@
-
 def test_log_linear():
+    import numpy as np
+    from sklearn.linear_model import LinearRegression
+
     from log_linear.log_linear import LogLinear
 
     ll = LogLinear()
+    lr = LinearRegression(fit_intercept=False)
 
-    
+    # np.random.seed(17)
+
+    n = 100
+    k = 10
+    k_sigma = 3
+    delta = []
+    for _ in range(100):
+        X = np.random.normal(size=(n, k))
+        X_sigma = np.random.normal(size=(n, k_sigma))
+
+        beta_sigma_0 = 0.1 + 0.9 * np.random.uniform()
+        beta_sigma = np.random.normal(size=(k_sigma,))
+        log_sigma2 = beta_sigma_0 + X_sigma @ beta_sigma
+
+        sigma = np.sqrt(np.exp(log_sigma2))
+        eps = sigma * np.random.normal(size=(n,))
+        beta = np.random.normal(size=(k,))
+        y = X @ beta + eps
+
+        ll.fit(X, X_sigma, y)
+        lr.fit(X, y)
+        delta.append(np.linalg.norm(beta - ll.coef_) - np.linalg.norm(beta - lr.coef_))
+
+    delta = np.array(delta)
+    mu = delta.mean()
+    se = delta.std() / np.sqrt(len(delta))
+    print(mu, mu / se)
